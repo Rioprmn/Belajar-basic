6aa4c203e0b689fb9f025d568a6984ed64a0588e
Rio Permana <githubrio@gmail.com>
2026-02-02 20:05:51 +0700

    js dasar

---
+++ JS dasar/latihan4.js
@@ -0,0 +1,10 @@
+var j = 10;
+var Beroprasi = 8;
+var i = 1;
+while (i <= Beroprasi){
+    console.log('Ojek NO.' +i + ' Beroprasi dengan baik.');
+    i++;
+}
+for(i = Beroprasi+1 ; i <= j; i++){
+    console.log('Ojek NO.' +i + ' Sedang Tidak Beroprasi.');
+}
---
+++ JS dasar/latihan5.js
@@ -0,0 +1,25 @@
+// var i = 3;
+// if (i === 1){
+//     alert('masukan angka 1');
+// }else{
+//     alert('angka yang anda masukan salah');
+// }
+
+
+// var i = 20;
+// if (i % 2== 0){
+//     alert(i + ' Genap');
+// }else{
+//     alert(i + ' Ganjil');
+// }
+
+var j = 10;
+var Beroprasi = 6;
+
+for(i = 1; i <= j; i++){
+    if (i <= Beroprasi){
+        console.log('Ojek NO.' +i + ' Beroprasi dengan baik.');
+    }else{
+        console.log('Ojek NO.' +i + ' Sedang Tidak Beroprasi.');
+    }   
+}
