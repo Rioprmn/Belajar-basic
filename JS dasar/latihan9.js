562c20315d903b81d7724f8b13328df7cc7cb2ce
Rio Permana <githubrio@gmail.com>
2026-02-04 19:15:58 +0700

    js dasar

---
+++ JS dasar/latihan9.js
@@ -0,0 +1,33 @@
+// var a=8;
+// var b=3;
+// var volumeA;
+// var volumeB;
+// var total;
+
+// volumeA = a * a * a;
+// volumeB = b * b * b;
+// total = volumeA + volumeB;
+
+// console.log("Volume A adalah " + volumeA);
+// console.log("Volume B adalah " + volumeB);
+// console.log("Total Volume A dan B adalah " + total);
+
+
+function jumlahVolumeKubus(a, b){
+    var volumeA;
+    var volumeB;
+    var total;
+
+    volumeA = a * a * a;
+    volumeB = b * b * b;
+    total = volumeA + volumeB;
+
+return total;
+
+}
+
+console.log(jumlahVolumeKubus(8, 3));
+console.log(jumlahVolumeKubus(11, 5));
+
+
+
