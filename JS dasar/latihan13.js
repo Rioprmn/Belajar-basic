95bfd6bcabcbb06d121b42b71c2bfb5ce0c3dc7d
Rio Permana <githubrio@gmail.com>
2026-02-06 09:32:29 +0700

    js dasar

--- JS dasar/latihan13.js
+++ JS dasar/latihan13.js
@@ -1,7 +1,42 @@
-var binatang = [];
-var binatang = ["kucing", "anjing", "kelinci", "hamster"];
-console.log (binatang[2]);
+// var binatang = [];
+// var binatang = ["kucing", "anjing", "kelinci", "hamster"];
+// console.log (binatang[2]);
 
-var angka = [1,2,3,[4,5]];
+// var angka = [1,2,3,[4,5]];
 
-console.log(angka[3][1]);
+// console.log(angka[3][1]);
+
+
+// var arr = [];
+// arr[0] = "satu";
+// arr[1] = "dua";
+// arr[2] = "tiga";
+
+// console.log(arr);
+
+
+// var arr = ["satu", "dua", "tiga"];
+// arr [1] = undefined;
+
+// console.log(arr);
+
+
+// var arr = ["Rio", "Budi", "Ani", "Siti"];
+//  for ( var i =0 ; i < arr.length; i++){
+//     console.log ('mahasiswa ke '+ (i+1) + ' adalah ' + arr[i]);
+//  }
+
+
+
+var arr = ["Rio", "Budi", "Ani", "Siti"];
+// console.log(arr.join(' - '));
+
+// arr.push ("Agus", "Lina");
+// arr.pop ();
+// console.log(arr.join(' - '));
+
+
+// arr.unshift("Agus");
+// arr.shift();
+console.log(arr.join(' - '));
+
