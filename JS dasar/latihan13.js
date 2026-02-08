a2d849242525d768414f50a88fe5828c0605becc
Rio Permana <githubrio@gmail.com>
2026-02-08 22:04:36 +0700

    js dasar

--- JS dasar/latihan13.js
+++ JS dasar/latihan13.js
@@ -28,7 +28,7 @@
 
 
 
-var arr = ["Rio", "Budi", "Ani", "Siti", "Lina"];
+// var arr = ["Rio", "Budi", "Ani", "Siti", "Lina"];
 // console.log(arr.join(' - '));
 
 // arr.push ("Agus", "Lina");
@@ -40,10 +40,44 @@ var arr = ["Rio", "Budi", "Ani", "Siti", "Lina"];
 // arr.shift();
 
 
-arr.splice (1,2);
+// arr.splice (1,2);
 
 
-var arr2= arr.slice (1,3); 
+// var arr2= arr.slice (1,3); 
 
-console.log(arr.join(' - '));
+// console.log(arr.join(' - '));
 
+
+
+// var angka = [1,2,3,4,5,6,7,8,9,10];
+// var nama = ["Rio", "Budi", "Ani", "Siti"];
+
+// angka.forEach(function(e){
+//     console.log(e);
+// });
+// var cetak = function(e){
+//     console.log(e);
+// }
+// angka.forEach(cetak);
+
+// nama.forEach(function(e, i){
+//     console.log('Mahasiswa ke ' + (i+1) + ' adalah ' + e);
+// });
+
+
+
+// var angka = [1,2,5,,3,6,8,4];
+// var angka2 = angka.map(function(e){
+//    return e * 2;
+// });
+
+// console.log(angka2.join(' - '));
+
+
+
+var angka = [1,2,5,7,3,19,11,26,8,4];
+console.log(angka.join(' - '));
+angka.sort(function(a,b){
+    return a - b;
+});
+console.log(angka.join(' - '));
