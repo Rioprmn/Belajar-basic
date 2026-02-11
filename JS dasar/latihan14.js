9ae5c8800484dbbe07235e9d5de3fccf4191ea00
Rio Permana <githubrio@gmail.com>
2026-02-11 07:00:57 +0700

    js dasar

---
+++ JS dasar/latihan14.js
@@ -0,0 +1,60 @@
+var penumpang = [];
+var tambahPenumpang = function (namaPenumpang, penumpang) {
+   // jika angkot kosong
+   if (penumpang.length == 0) {
+        // tambah penumpang di awal array
+        penumpang.push(namaPenumpang);
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+   }else{
+    // telusuri seluruh kursi dari awal
+    for (var i = 0; i < penumpang.length; i++) {
+        //jika ada kursi kosong
+        if (penumpang[i] == undefined) {
+            // tambah penumpang di kursi tersebut
+            penumpang[i] = namaPenumpang;
+            //kembalikan isi array & keluar dari function
+            return penumpang;
+        }
+        //jika sudah ada nama yang sama
+        else if (penumpang[i] == namaPenumpang) {
+            //tampilkan pesan kesalahan
+            console.log(namaPenumpang + ' sudah ada di dalam angkot.');
+        //tampilkan pesan kesalahan
+            return penumpang;
+        }
+        //jika seluruh kursi terisi
+        else if (i == penumpang.length - 1) {
+            // tambah penumpang di akhir array
+            penumpang.push(namaPenumpang);
+            // kembalikan isi array & keluar dari function
+            return penumpang;
+        }
+   }
+}
+
+}
+var hapusPenumpang = function (namaPenumpang, penumpang) {
+    // jika angkot kosong
+    if (penumpang.length == 0) {
+        // tampilkan pesan bahwa angkot kosong
+        console.log('Angkot masih kosong.');
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+    }else{
+        // telusuri seluruh kursi dari awal
+        for (var i = 0; i < penumpang.length; i++) {
+            // jika nama penumpang sesuai
+                if (penumpang[i] == namaPenumpang) {
+                    // hapus penumpang dengan mengubah namanya menjadi undefined
+                    penumpang[i] = undefined;
+                    // kembalikan isi array & keluar dari function
+                    return penumpang;
+                }else if (i == penumpang.length -1) {
+                    // tampilkan pesan bahwa nama penumpang tidak ditemukan
+                    console.log(namaPenumpang + ' tidak ada di dalam angkot.');
+                }
+    }
+}
+return penumpang;
+}
