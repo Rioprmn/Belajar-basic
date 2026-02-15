b0e44dd19a069fb51b3698633af880113b820faa
Rio Permana <githubrio@gmail.com>
2026-02-15 11:59:24 +0700

    js dasar

---
+++ JS dasar/latihan16.js
@@ -0,0 +1,56 @@
+//membuat object
+//cara ke 1 fusction declaration
+// function hallo(){
+//     console.log("hallo");
+// }
+// hallo();
+
+
+//cara ke 2 object literal
+// var object= {};
+// object.halo = function(){
+//     console.log("hallo");
+// } 
+// object.halo();
+
+
+
+// cara ke 3 constructor function
+// function hallo (){
+//     console.log("hallo");
+// }
+// new hallo();
+
+
+
+// konsep this
+// var a = 10;
+// console.log(this.a);
+
+
+
+// function hallo(){
+//     console.log(this);
+//     console.log("hallo");
+// }
+// this.hallo();
+
+
+
+// var object= {a : 10, nama : 'Rio'};
+// object.halo = function(){
+//     console.log(this);
+//     console.log("hallo");
+// } 
+// object.halo();
+
+
+
+
+
+function hallo (){
+    console.log(this);
+    console.log("hallo");
+}
+var obj1 =new hallo();
+var obj2 = new hallo();
